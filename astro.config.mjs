4bbfd024405cb6685732da35a8a471ed24655e0b
zooeywm <zooeywm@gmail.com>
2026-01-12 10:50:06 +0800

    Initial commit from zooeywm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import d2 from "astro-d2";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  base: '/knowledge-base/',
+  integrations: [
+    starlight({
+      title: "zooeywm 知识库",
+      social: [{ icon: "github", label: "GitHub", href: "https://github.com/zooeywm" }],
+      sidebar: [
+        {
+          label: "mdx 小抄",
+          autogenerate: { directory: "misc" },
+        },
+        { label: "编程语言", autogenerate: { directory: "languages" } },
+      ],
+      locales: {
+        root: {
+          label: '简体中文',
+          lang: 'zh-CN',
+        },
+      },
+    }),
+    d2(),
+  ],
 });
